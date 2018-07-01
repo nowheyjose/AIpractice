d7b7909e7aae62f30e6a47db052a5ef76ce5819b
Jose-Rafael <jdiaz5511@gmail.com>
2018-07-01 17:49:50 -0400

    dfs graph traversal

--- search/search.py
+++ search/search.py
@@ -71,6 +71,39 @@ def tinyMazeSearch(problem):
     s = Directions.SOUTH
     w = Directions.WEST
     return  [s, s, w, s, w, w, s, w]
+'''
+def dfsUtil(problem, v, visited):
+    visited[v] = True
+    print v,
+
+    for i in problem:
+        if visited[v] = False:
+            self.dfsUtil(problem.getSuccessor,v, visited)
+'''
+#this general helper function referenced from kevinkoo001
+#helped me figure out how to keep track of the paths traversed
+#using a dictionary
+#the solution reduces memory usage by creating a single parent array
+def getPath(goal, prevNodes, nxtMoves):
+    answer = []
+    currVertex = goal
+
+    #traverse through previous vertices
+    while currVertex in prevNodes:
+        #add set of directions to answer while traversing nodes
+        answer.append(nxtMoves[currVertex])
+        #set next coordinate to find the direction taken to
+        #reach it
+        currVertex = prevNodes[currVertex]
+
+    #the path is appended in reverse since the start was
+    #the final node/goal
+    #we need to flip the path so it reads the correct answer
+    answer.reverse()
+
+    return answer
+
+
 
 def depthFirstSearch(problem):
     """
@@ -93,7 +126,38 @@ def depthFirstSearch(problem):
     Is the start a goal? False
     Start's successors: [((5, 4), 'South', 1), ((4, 5), 'West', 1)]
     """
-    
+
+    start = problem.getStartState()
+    mazeFringe = util.Stack()
+    #track visited nodes in maze
+    visited = []
+    #util.Stack push(self,item), pushes the 'item' to mazeFringe
+    mazeFringe.push(start)
+
+    #create dictionaries that can store pacman's movement
+    parentPath = {}
+    pastMoves = {}
+
+    #stop if already at goal, there's nothing to return
+    if problem.isGoalState(start):
+        return visited
+
+    while not mazeFringe.isEmpty():
+        #need to know the current state and next move of agent
+        currLocation = mazeFringe.pop()
+        #add visited nodes in maze to visited
+        visited.append(currLocation)
+        for (nxtMove, direction, cost) in problem.getSuccessors(currLocation):
+            if nxtMove not in visited:
+                visited.append(nxtMove)
+                #record path and direction taken
+                parentPath[nxtMove] = currLocation
+                pastMoves[nxtMove] = direction
+                if problem.isGoalState(nxtMove):
+                    return getPath(nxtMove, parentPath, pastMoves)
+                mazeFringe.push(nxtMove)
+
+    #the search failed, return undefined
     util.raiseNotDefined()
 
 def breadthFirstSearch(problem):
